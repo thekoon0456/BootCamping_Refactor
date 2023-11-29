d43c138fed5dd225cdd106ae909cb0bf8f2cbef7
thekoon0456 <thekoon0456@gmail.com>
2023-11-29 17:47:02 +0900

    [Refactor] DiaryView 코드 리팩토링

--- BootCamping/BootCamping/Views/MyCampingDiary/MyCampingDiaryView.swift
+++ BootCamping/BootCamping/Views/MyCampingDiary/MyCampingDiaryView.swift
@@ -6,20 +6,19 @@
 //
 
 import SwiftUI
+
+import AlertToast
 import Firebase
 import FirebaseFirestore
-import AlertToast
-
 
 struct MyCampingDiaryView: View {
+    
     @EnvironmentObject var diaryStore: DiaryStore
     @EnvironmentObject var faceId: FaceId
+    @EnvironmentObject var tabSelection: TabSelector
     
     @StateObject var diaryLikeStore: DiaryLikeStore = DiaryLikeStore()
     @StateObject var commentStore: CommentStore = CommentStore()
-    
-    @EnvironmentObject var tabSelection: TabSelector
-    
     @AppStorage("faceId") var usingFaceId: Bool? //페이스id 설정 사용하는지
     
     @State private var isShowingAcceptedToast = false
@@ -32,16 +31,20 @@ struct MyCampingDiaryView: View {
                 ScrollView(showsIndicators: false) {
                     LazyVStack {
                         ForEach(diaryStore.myDiaryUserInfoDiaryList, id: \.self) { userInfoDiary in
-                            DiaryCellView(item: userInfoDiary, isShowingAcceptedToast: $isShowingAcceptedToast, isShowingBlockedToast: $isShowingBlockedToast)
+                            DiaryCellView(item: userInfoDiary,
+                                          isShowingAcceptedToast: $isShowingAcceptedToast,
+                                          isShowingBlockedToast: $isShowingBlockedToast)
                                 .task {
-                                    guard let index = diaryStore.myDiaryUserInfoDiaryList.firstIndex(where: { $0.diary.id == userInfoDiary.diary.id}) else { return }
-
+                                    guard let index = diaryStore
+                                        .myDiaryUserInfoDiaryList
+                                        .firstIndex(where: { $0.diary.id == userInfoDiary.diary.id})
+                                    else { return }
+                                    
                                     if (index + 1) % 5 == 0 {
                                         Task {
                                             diaryStore.nextGetMyDiaryCombine()
                                         }
                                     }
-                                    
                                 }
                         }
                     }
@@ -58,8 +61,6 @@ struct MyCampingDiaryView: View {
                     //탭틱
                     UIImpactFeedbackGenerator(style: .rigid).impactOccurred()
                 }
-
-                
             }
             //다이어리 비어있을때 추가 화면
             DiaryEmptyView().zIndex(-1)
@@ -76,12 +77,6 @@ struct MyCampingDiaryView: View {
                     .font(.title2.bold())
             }
             ToolbarItem(placement: .navigationBarTrailing) {
-                //기존 코드
-//                NavigationLink (destination: DiaryAddView())
-//                {
-//                    Image(systemName: "plus")
-//                }
-                
                 //Add View 모달로 변경
                 Button {
                     self.isShowingAdd = true
@@ -89,15 +84,13 @@ struct MyCampingDiaryView: View {
                     Image(systemName: "square.and.pencil")
                         .font(.title3.bold())
                 }
-//                .sheet(isPresented: self.$isShowingAdd) {
-//                    DiaryAddView()
-//                }
                 .fullScreenCover(isPresented: $isShowingAdd) {
                     DiaryAddView(isShowingAdd: $isShowingAdd)
                 }
             }
         }
-        .alert("노트 만들기에 실패했습니다.. 다시 시도해 주세요.", isPresented: $diaryStore.isError) {
+        .alert("노트 만들기에 실패했습니다.. 다시 시도해 주세요.",
+               isPresented: $diaryStore.isError) {
             Button("확인", role: .cancel) {
                 diaryStore.isError = false
             }
